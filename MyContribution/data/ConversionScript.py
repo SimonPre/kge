3c69a260fd2c5aae791a3b2b41f2f48ae1a2261f
simon <simon.pressler@web.de>
2021-11-27 15:04:05 +0100

    ConversionScript.py done, works

--- MyContribution/data/ConversionScript.py
+++ MyContribution/data/ConversionScript.py
@@ -20,14 +20,15 @@ def convert_text_to_del(relation_ids_path: str,
     relations_lists = convert_to_nested_lists(relation_ids_path)
     relations_dict = convert_to_dict(relations_lists)
 
-    node_list = convert_to_nested_lists(node_ids_path)
-    node_dict = {tuple_[1]: tuple_[0] for tuple_ in node_list if len(tuple_) > 1}
+    nodes_lists = convert_to_nested_lists(node_ids_path)
+    nodes_dict = convert_to_dict(nodes_lists)
+
 
     with open(f'{text_file_to_convert_path}') as f:
         train = f.read()
         train_triples = [line.split("\t") for line in train.split("\n")]
         triples_with_numeric_replacement = [
-            [node_dict[triple[0]], relations_dict[triple[1]], node_dict[triple[2]]]
+            [nodes_dict[triple[0]], relations_dict[triple[1]], nodes_dict[triple[2]]]
             for triple
             in train_triples
             if len(triple) > 2
@@ -44,7 +45,11 @@ def convert_dict_to_(path_to_dict: str, type_to_convert_to: str):
         if type_to_convert_to == "txt":
             entities = re.sub("\d*\t", "", entities)
         if type_to_convert_to == "del":
-            path_to_dict = path_to_dict.replace("s.", "ids.")
+            if "entities" in path_to_dict:
+                path_to_dict = path_to_dict.replace("ies.", "y_ids.")
+            elif "relations" in path_to_dict:
+                path_to_dict = path_to_dict.replace("s.", "_ids.")
+
         if type_to_convert_to not in ["del", "txt"]:
             print(f"{type_to_convert_to} not accepted. Choose between txt and del")
             raise ValueError
@@ -58,32 +63,35 @@ def count_elements_in_file(path: str):
 
 
 def create_dataset_yaml(path: str, name: str):
-    output_dict = {"name": name,
-                   "num_entities": count_elements_in_file(f"{path}/entities.txt"),
-                   "num_relations": count_elements_in_file(f"{path}/relations.txt")}
+    inner_dict = {"name": name,
+                  "num_entities": count_elements_in_file(f"{path}/entities.txt"),
+                  "num_relations": count_elements_in_file(f"{path}/relations.txt")}
 
     for split_type in ["test", "train", "valid"]:
         # toDO: Need to figure out "without_unseen" means, and how to sample
-        output_dict[f"file.{split_type}.type"] = "triples"
-        output_dict[f"file.{split_type}.filename"] = f"{split_type}.dat"
-        output_dict[f"file.{split_type}.split_type"] = split_type
-        output_dict[f"file.{split_type}.size"] = count_elements_in_file(f"{path}/{split_type}.dat")
+        inner_dict[f"file.{split_type}.type"] = "triples"
+        inner_dict[f"file.{split_type}.filename"] = f"{split_type}.del"
+        inner_dict[f"file.{split_type}.split_type"] = split_type
+        inner_dict[f"file.{split_type}.size"] = count_elements_in_file(f"{path}/{split_type}.del")
 
-    with open(f'{path}/dataset.yml', 'w') as outfile:
+    output_dict = {"dataset": inner_dict}
+    with open(f'{path}/dataset.yaml', 'w') as outfile:
         yaml.dump(output_dict, outfile, default_flow_style=False)
 
 
 if __name__ == '__main__':
     cwd = os.getcwd()
-    for file in [("entities", "del"), ("relations", "del"), ("relations", "txt"), ("entities", "txt")]:
-        convert_dict_to_(f'{cwd}/DDB14/{file[0]}.dict', file[1])
+    for dataset in ["DDB14", "NELL995"]:
+        print(dataset)
+        for file in [("entities", "del"), ("relations", "del"), ("relations", "txt"), ("entities", "txt")]:
+            convert_dict_to_(f'{cwd}/{dataset}/{file[0]}.dict', file[1])
 
-    for file in ["test", "train", "valid"]:
-        numeric = convert_text_to_del(relation_ids_path=f'{cwd}/DDB14/relations.dict',  #
-                                      node_ids_path=f'{cwd}/DDB14/entities.dict',
-                                      text_file_to_convert_path=f'{cwd}/DDB14/{file}.txt')
-        dat_file = open(f"DDB14/{file}.dat", "w")
-        dat_file.write(numeric)
-        dat_file.close()
+        for file in ["test", "train", "valid"]:
+            numeric = convert_text_to_del(relation_ids_path=f'{cwd}/{dataset}/relations.dict',
+                                          node_ids_path=f'{cwd}/{dataset}/entities.dict',
+                                          text_file_to_convert_path=f'{cwd}/{dataset}/{file}.txt')
+            dat_file = open(f"{dataset}/{file}.del", "w")
+            dat_file.write(numeric)
+            dat_file.close()
 
-    create_dataset_yaml(path=f'{cwd}/DDB14', name='DDB14')
+        create_dataset_yaml(path=f'{cwd}/{dataset}', name=dataset)
